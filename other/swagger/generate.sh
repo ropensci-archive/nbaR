f464e3e1c4f89da77e52363e85b04bac6146fe77
hettling <hannes.hettling@gmail.com>
2018-03-28 18:11:26 +0200

    adding template dir

--- other/swagger/generate.sh
+++ other/swagger/generate.sh
@@ -1,1 +1,2 @@
-java -jar ~/git/swagger-codegen/modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate -c ~/git/nbaR/other/swagger/config.json -i  http://localhost:8080/v2/reference-doc -l r -o ~/git/nbaR
+#!/bin/sh
+java -jar ~/git/swagger-codegen/modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate -c ~/git/nbaR/other/swagger/config.json -i  http://localhost:8080/v2/reference-doc -l r  -t ~/git/nbaR/other/swagger/swagger-templates/ -o ~/git/nbaR
