48f4a0cf713431ccb23b3227868b86739aa8f8d9
hettling <hannes.hettling@gmail.com>
2018-07-24 14:40:28 +0200

    adding tests

---
+++ tests/testthat/test-geoClient-metadata.R
@@ -0,0 +1,40 @@
+library(nbaR)
+library(testthat)
+
+wd <- getwd()
+if(grepl('testthat', wd)) {
+    dataDir <- file.path('data')
+} else {
+    ## for running test at package level
+    dataDir <- file.path('tests', 'testthat', 'data')
+}
+
+gc <- GeoClient$new(basePath="http://api.biodiversitydata.nl/v2")
+
+context("Testing GeoClient metadata endpoints")
+
+test_that("getPaths works", {
+    res <- gc$get_paths()
+    expect_is(res$content, "character")
+    expect_true(length(res$content) > 0)    
+})
+
+test_that("getFieldInfo works", {
+    res <- gc$get_field_info()
+    list <- res$content
+    expect_is(list, "list")
+    ## The list should be named by the paths of the different fields, compare them
+    paths <- gc$get_paths()$content
+    expect_equal(sort(paths), sort(names(list)))
+})
+
+test_that("GetSettings works", {
+    res <- gc$get_settings()
+    expect_true(! is.null(res$content))    
+})
+
+test_that("GetSetting works", {
+    for (setting in names(gc$get_settings()$content)) {
+        expect_true(! is.null(gc$get_setting(setting)$content))
+    }
+})
