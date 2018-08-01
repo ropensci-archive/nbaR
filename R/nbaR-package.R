76d59a64a2612e789d480844f4c385f2e3cbfc07
hettling <hannes.hettling@gmail.com>
2018-08-01 16:50:52 +0200

    adding package docu file

---
+++ R/nbaR-package.R
@@ -0,0 +1,11 @@
+#' nbaR : R Package Client for the Netherlands Biodiversity Api
+#'
+#' Access to the digitised Natural History collection at the Naturalis Biodiversity Center
+#'
+#' @name nbaR
+#' @aliases nbaR-package
+#' @title nbaR : R Package Client for the Netherlands Biodiversity Api
+#' @docType package
+#' @keywords package
+#' @author Hannes Hettling \email{ hannes.hettling@@naturalis.nl }
+NULL
