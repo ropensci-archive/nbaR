5f03c7cea15ccf45b92740350c5a9578a7e48f9a
hettling <hannes.hettling@gmail.com>
2018-10-04 16:43:22 +0200

    avoid long code lines

--- R/Utils.r
+++ R/Utils.r
@@ -1,5 +1,5 @@
 #' @importFrom stats aggregate complete.cases
-#' @importFrom ape getMRCA read.nexus
+#' @importFrom ape getMRCA read.nexus makeChronosCalib
 
 #' @name geo_age
 #' @family utils
@@ -16,11 +16,13 @@
 #' geo_age('lower miocene')
 #' @export
 geo_age <- function(geo_time) {
-  as.data.frame(vapply(geo_time, .geo_age, FUN.VALUE = list(early_age = NA, late_age = NA)))
+  as.data.frame(vapply(geo_time, .geo_age,
+                       FUN.VALUE = list(early_age = NA, late_age = NA)))
 }
 
 #' @noRd
-#' Internal Function to get a geological age range from unit.
+#' Internal Function to get a geological age range from unit,
+#' calling the API from earthlifeconsortium
 .geo_age <- function(geo_time) {
   if (is.null(geo_time)) {
     return(list(early_age = NA, late_age = NA))
@@ -99,20 +101,38 @@ chronos_calib <- function(specimens, tree, level = "genus") {
     if (is.null(family)) family <- NA
     genus <- x$identifications[[1]]$defaultClassification$genus
     if (is.null(genus)) genus <- NA
-    specificEpithet <- x$identifications[[1]]$defaultClassification$specificEpithet
+    specificEpithet <-
+        x$identifications[[1]]$defaultClassification$specificEpithet
     if (is.null(specificEpithet)) specificEpithet <- NA
     youngChronoName <- x$gatheringEvent$chronoStratigraphy[[1]]$youngChronoName
     if (is.null(youngChronoName)) youngChronoName <- NA
     oldChronoName <- x$gatheringEvent$chronoStratigraphy[[1]]$oldChronoName
     if (is.null(oldChronoName)) oldChronoName <- NA
-    c(order = order, family = family, genus = genus, specificEpithet = specificEpithet, youngChronoName = youngChronoName, oldChronoName = oldChronoName)
+    c(order = order, family = family, genus = genus,
+      specificEpithet = specificEpithet,
+      youngChronoName = youngChronoName,
+      oldChronoName = oldChronoName)
   }))
   data <- as.data.frame(data)
   ## translate the geological periods to mya
-  times <- geo_age(unique(c(as.character(data$youngChronoName), as.character(data$oldChronoName))))
+  times <- geo_age(unique(c(as.character(data$youngChronoName),
+                            as.character(data$oldChronoName))))
 
-  data$young_age <- vapply(data$youngChronoName, function(x) ifelse(is.na(x), NA, unlist(times["late_age", as.character(x)])), FUN.VALUE=numeric(1))
-  data$old_age <- vapply(data$oldChronoName, function(x) ifelse(is.na(x), NA, unlist(times["early_age", as.character(x)])), FUN.VALUE=numeric(1))
+  data$young_age <- vapply(data$youngChronoName,
+      function(x){
+        ifelse(is.na(x),
+               NA,
+               unlist(times["late_age", as.character(x)]))
+      },
+      FUN.VALUE=numeric(1))
+  
+  data$old_age <- vapply(data$oldChronoName,
+      function(x){
+        ifelse(is.na(x),
+               NA,
+               unlist(times["early_age", as.character(x)]))
+      },
+      FUN.VALUE=numeric(1))
 
   ## filter for the ones that have data
   data <- data[!(is.na(data$young_age) & is.na(data$old_age)), ]
@@ -120,19 +140,25 @@ chronos_calib <- function(specimens, tree, level = "genus") {
   ## filter out duplicates
   data <- unique(data)
 
-  tree_genera <- vapply(strsplit(tree$tip.label, "_"), `[`, 1, FUN.VALUE=character(1))
-  tree_species <- vapply(strsplit(tree$tip.label, "_"), `[`, 2, FUN.VALUE=character(1))
+  tree_genera <- vapply(strsplit(tree$tip.label, "_"),
+                        `[`, 1, FUN.VALUE=character(1))
+  tree_species <- vapply(strsplit(tree$tip.label, "_"),
+                        `[`, 2, FUN.VALUE=character(1))
 
   tree_taxa <- vector()
   if (level == "genus") {
     tree_taxa <- tree_genera
   } else {
-    tree_taxa <- as.character(mapply(function(x, y) get_higher_taxon(x, y, level), tree_genera, tree_species))
+      tree_taxa <- as.character(mapply(function(x, y)
+          get_higher_taxon(x, y, level), tree_genera, tree_species))
   }
   ## filter out taxa that are not in our tree
   data_filtered <- data[data[, level] %in% tree_taxa, ]
 
-  taxa_ages <- aggregate(data_filtered[c("young_age", "old_age")], by = list(data_filtered[, level]), FUN = mean, na.rm = T)
+  taxa_ages <- aggregate(data_filtered[c("young_age", "old_age")],
+                         by = list(data_filtered[, level]),
+                         FUN = mean,
+                         na.rm = TRUE)
   taxa_ages <- taxa_ages[complete.cases(taxa_ages), ]
   if (nrow(taxa_ages) == 0) {
     warning("Could not find calibration points")
@@ -148,7 +174,10 @@ chronos_calib <- function(specimens, tree, level = "genus") {
   }, FUN.VALUE=numeric(1))
 
   taxa_ages <- taxa_ages[complete.cases(taxa_ages), ]
-  calib <- makeChronosCalib(tree, taxa_ages$node, age.min = taxa_ages$young_age, age.max = taxa_ages$old_age)
+  calib <- makeChronosCalib(tree,
+                            taxa_ages$node,
+                            age.min = taxa_ages$young_age,
+                            age.max = taxa_ages$old_age)
 
   ## add group that will be calibrated to calibration point data frame
   calib <- cbind(calib, taxon = taxa_ages[, 1])
@@ -159,14 +188,18 @@ chronos_calib <- function(specimens, tree, level = "genus") {
 #' Internal Function to get the higher-level taxon of a species,
 #' given its genus and species name
 get_higher_taxon <- function(genus, specificEpithet, rank) {
-  tc <- TaxonClient$new(basePath = "http://145.136.242.167:8080/v2")
-  res <- tc$query(queryParams = list(acceptedName.genusOrMonomial = genus, acceptedName.specificEpithet = specificEpithet))
+  tc <- TaxonClient$new()
+    res <- tc$query(
+        queryParams = list(acceptedName.genusOrMonomial = genus,
+                           acceptedName.specificEpithet = specificEpithet))
   if (res$content$totalSize == 0) {
-    warning("Could not get higher level taxon for: ", genus, "", specificEpithet)
+      warning("Could not get higher level taxon for: ",
+              genus, "", specificEpithet)
     return(NA)
   }
   if (res$content$totalSize > 1) {
-    warning("More than one taxon record found for : ", genus, specificEpithet, ". Taking first hit")
+      warning("More than one taxon record found for : ",
+              genus, specificEpithet, ". Taking first hit")
   }
   result <- res$content$resultSet[[1]]$item$defaultClassification[[rank]]
   result
