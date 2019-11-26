755e8e9ebb551c53154518060a2b5627f10ae0ac
hettling <hannes.hettling@gmail.com>
2019-11-26 15:45:17 +0100

    fixing tests, but still errors

--- tests/testthat/test-multimediaClient-find.R
+++ tests/testthat/test-multimediaClient-find.R
@@ -3,7 +3,7 @@ source("setup-vars.R")
 context("Testing multimedia find functions")
 
 test_that("find_by_ids() function works", {
-  ids <- "ZMA.MAM.29046_lat@CRS,L.1454647_0188954023@BRAHMS"
+  ids <- "RMNH.CRUS.D.17131_0@CRS,L.1454647_0188954023@BRAHMS"
   res <- mc$find_by_ids(ids)
   expect_is(res$content, "list")
   expect_length(res$content, 2)
@@ -11,14 +11,14 @@ test_that("find_by_ids() function works", {
     expect_is(res$content[[i]], "MultiMediaObject")
   }
 
-  id <- "ZMA.MAM.29046_lat@CRS"
+  id <- "RMNH.CRUS.D.17131_0@CRS"
   res <- mc$find_by_ids(id)
   expect_is(res$content, "list")
   expect_length(res$content, 1)
   expect_is(res$content[[1]], "MultiMediaObject")
 
   ## check if find_by_ids works with vector instead of string
-  ids_vec <- c("ZMA.MAM.29046_lat@CRS", "L.1454647_0188954023@BRAHMS")
+  ids_vec <- c("RMNH.CRUS.D.17131_0@CRS", "L.1454647_0188954023@BRAHMS")
   res <- mc$find_by_ids(ids_vec)
   expect_is(res$content, "list")
   expect_length(res$content, 2)
