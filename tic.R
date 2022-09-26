f6c2c5f3a16f2e83d6bcfedf7a27ea9e4c123d4f
Kevin Cazelles <kevin.cazelles@gmail.com>
2022-09-25 20:21:01 -0400

    CI switched to Github Actions

--- tic.R
+++
@@ -1,8 +0,0 @@
-if (Sys.getenv("BUILD_PKGDOWN") != "" &&
-    Sys.getenv("TRAVIS_EVENT_TYPE") != "cron" &&
-    Sys.getenv("TRAVIS_R_VERSION_STRING") == "devel") {
-    get_stage("deploy") %>%
-        add_step(step_build_pkgdown()) %>%
-        add_step(step_push_deploy())
-}
-
